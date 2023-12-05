9b9c3488ff92f09fe3197575ccbbf5e08d81de0b
viljinsky <viljinsky@narod.ru>
2023-12-05 08:52:51 +0300

    initial commit

---
+++ src/ru/viljinsky/puzzle5/Slicer.java
@@ -0,0 +1,140 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ru.viljinsky.puzzle5;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Shape;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.QuadCurve2D;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author viljinsky
+ */
+public class Slicer extends ArrayList<Shape> {
+
+    int cols;
+    int rows;
+
+    private Shape horizontal(Point p1, Point p2, int s) {
+        int ctrlx = (p1.x + p2.x) / 2;
+        int ctrly = (p1.y + p2.y) / 2 + 10 * s;
+        return new QuadCurve2D.Float(p1.x, p1.y, ctrlx, ctrly, p2.x, p2.y);
+    }
+
+    private Shape vertical(Point p1, Point p2, int s) {
+        int ctrlx = (p1.x + p2.x) / 2 + 10 * s;
+        int ctrly = (p1.y + p2.y) / 2;
+        return new QuadCurve2D.Float(p1.x, p1.y, ctrlx, ctrly, p2.x, p2.y);
+    }
+
+    public Shape shape(int col, int row) {
+        int n;
+        GeneralPath path = new GeneralPath();
+        if (col % 2 + row % 2 == 1) {
+            n = col * rows * 2 + row * 2;
+
+            path.append(get(n), true);
+            path.append(get(n + 1), true);
+
+            n = col * rows * 2 + (row + 1) * 2;
+            path.append(get(n), true);
+
+            n = (col + 1) * rows * 2 + row * 2 + 1;
+            path.append(get(n), true);
+
+        } else {
+
+            n = (col + 1) * rows * 2 + row * 2 + 1;
+            path.append(get(n), true);
+
+            n = col * rows * 2 + (row + 1) * 2;
+            path.append(get(n), true);
+
+            n = col * rows * 2 + row * 2;
+
+            path.append(get(n + 1), true);
+            path.append(get(n), true);
+
+        }
+
+        return path;
+    }
+
+    protected void paint(Graphics g) {
+        
+        Graphics2D g2 = (Graphics2D)g;
+        for (int col = 0; col < cols - 1; col++) {
+            for (int row = 0; row < rows - 1; row++) {
+                Shape s = shape(col, row);
+                g2.setColor(Color.red);
+                g2.fill(s);
+                g2.setColor(Color.blue);
+                g2.draw(s);
+            }
+        }
+    }
+    
+    public Slicer(Puzzle puzzle) {
+        cols = puzzle.cols + 1;
+        rows = puzzle.rows + 1;
+        for (int col = 0; col < cols; col++) {
+            for (int row = 0; row < rows; row++) {
+                Point p = new Point(col * Puzzle.COL_SIZE, row * Puzzle.ROW_SIZE);
+                Point p1 = new Point(p.x + Puzzle.COL_SIZE, p.y);
+                Point p2 = new Point(p.x, p.y + Puzzle.ROW_SIZE);
+
+                int s1 = (int) (Math.signum(0.5 - Math.random()));
+                int s2 = (int) (Math.signum(0.5 - Math.random()));
+
+                if (row % 2 + col % 2 == 1) {
+                    add(horizontal(p1, p, (row == 0 || row == puzzle.rows) ? 0 : s1));
+                    add(vertical(p, p2, col == 0 || col == puzzle.cols ? 0 : s2));
+                } else {
+                    add(horizontal(p, p1, (row == 0 || row == puzzle.rows) ? 0 : s1));
+                    add(vertical(p2, p, col == 0 || col == puzzle.cols ? 0 : s2));
+                }
+            }
+        }
+    }
+
+    public void showInFrame(Component parent) {
+        JPanel panel = new JPanel(){
+            @Override
+            public void paint(Graphics g) {
+                super.paint(g);
+                Slicer.this.paint(g);
+            }
+            
+        };
+        panel.setPreferredSize(new Dimension((cols-1)*Puzzle.COL_SIZE, (rows-1)*Puzzle.ROW_SIZE));
+        JFrame frame = new JFrame("Slicer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(panel);
+        frame.pack();
+                
+        frame.setLocationRelativeTo(parent);
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+
+        Puzzle puzzle = new Puzzle(12, 17);
+        Slicer graph = new Slicer(puzzle);
+        graph.showInFrame(null);
+
+    }
+
+
+}
--- src/ru/viljinsky/puzzle5/Test2.java
+++ src/ru/viljinsky/puzzle5/Test2.java
@@ -14,123 +14,9 @@ import java.awt.Image;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Shape;
-import java.awt.geom.GeneralPath;
-import java.awt.geom.QuadCurve2D;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
 import javax.swing.JPanel;
-
-class PuzzleSlicing extends ArrayList<Shape> {
-
-    ArrayList<Point> points = new ArrayList<>();
-
-    Point nextRight(Point point) {
-        Point result = null;
-        for (Point p : points) {
-            if (p.y == point.y && p.x > point.x) {
-                if (result == null || result.x > p.x) {
-                    result = p;
-                }
-            }
-        }
-        return result;
-    }
-
-    Point nextDown(Point point) {
-        Point result = null;
-        for (Point p : points) {
-            if (p.x == point.x && p.y > point.y) {
-                if (result == null || result.y > p.y) {
-                    result = p;
-                }
-            }
-        }
-        return result;
-    }
-
-    Shape horizontal(Point p1, Point p2, int ctrl) {
-
-        int k = (int) Math.signum(0.5 - Math.random());
-        int ctrlx = (p1.x + p2.x) / 2;
-        int ctrly = p1.y + ctrl * k;
-        return new QuadCurve2D.Float(p1.x, p1.y, ctrlx, ctrly, p2.x, p2.y);
-
-    }
-
-    Shape vertical(Point p1, Point p2, int ctrl) {
-        int k = (int) Math.signum(0.5 - Math.random());
-        int ctrlx = p1.x + ctrl * k;
-        int ctrly = (p1.y + p2.y) / 2;
-        return new QuadCurve2D.Float(p1.x, p1.y, ctrlx, ctrly, p2.x, p2.y);
-    }
-
-    public Shape shape(int col, int row) {
-        Shape s;
-        GeneralPath path = new GeneralPath();
-        
-        s = get((row*(cols) + col)*2);
-        path.append(s, false);
-        
-        s = get((row*(cols) + col+1)*2+1);
-        path.append(s, false);
-        
-      //  GeneralPath tmp = new GeneralPath();
-       
-
-        s = get(((row+1)*(cols) + col)*2);
-       
-        path.append(s, false);
-        
-        s = get((row*(cols) + col)*2+1);
-        path.append(s, false);
-        
-        
-        
-        return path;
-    }
-
-    int cols;
-    public PuzzleSlicing(Puzzle puzzle) {
-        cols = puzzle.cols+1;
-        for (int row = 0; row < puzzle.rows + 1; row++) {
-            for (int col = 0; col < puzzle.cols + 1; col++) {
-                Point p = new Point(col * Puzzle.COL_SIZE, row * Puzzle.ROW_SIZE);
-                points.add(p);
-                Point p1;
-                p1 = new Point(p.x+Puzzle.COL_SIZE,p.y);
-                add(horizontal(p, p1, (row == 0 || row == puzzle.rows) ? 0 : 10));                
-                p1 = new Point(p.x,p.y+Puzzle.ROW_SIZE);
-                add(vertical(p, p1, (col == 0 || col == puzzle.cols) ? 0 : 10));
-            }
-        }
-    }
-
-    public void paint(Graphics g) {
-        for (Point p : points) {
-            g.drawOval(p.x - 3, p.y - 3, 6, 6);
-        }
-
-        Graphics2D g2 = (Graphics2D) g;
-
-        System.out.println("--");
-        
-        g2.setColor(Color.red);
-        for(int i=0;i<size()/2;i++){
-            System.out.println(i*2);
-            g2.draw(get(i*2));
-        }
-        
-        g2.setColor(Color.blue);
-        for(int i=0;i<size()/2;i++){
-            System.out.println(i*2+1);
-            g2.draw(get(i*2+1));
-        }
-
-        System.out.println("--");
-
-    }
-
-}
 
 class PuzzleImage2 extends BufferedImage {
 
@@ -158,18 +44,18 @@ class PuzzlePanel2 extends JPanel {
     }
 
     Puzzle puzzle;
-    Image img;
-    PuzzleSlicing slicing;
+    BufferedImage img;
+    Slicer slicer;
 
     public PuzzlePanel2() {
 
         img = new PuzzleImage2(400, 500);
-        
+
         int cols = img.getWidth(null) / Puzzle.COL_SIZE;
         int rows = img.getHeight(null) / Puzzle.ROW_SIZE;
-        
+
         puzzle = new Puzzle(cols, rows);
-        slicing = new PuzzleSlicing(puzzle);
+        slicer = new Slicer(puzzle);
 
     }
 
@@ -177,22 +63,33 @@ class PuzzlePanel2 extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         
-        g.drawImage(img, 0, 0, this);
         
-        Graphics2D g2 = (Graphics2D)g;
-        for(int col =0;col<puzzle.cols;col++){
-            for(int row=0;row<puzzle.rows;row++){
-                if(col==4 && row==6){
-                g2.draw(slicing.shape(col, row));
-                }
+
+        Graphics2D g2 = (Graphics2D) g;
+        g2.drawImage(img, null, this);
+        AffineTransform old = g2.getTransform();
+        for (int col = 0; col < puzzle.cols; col++) {
+            for (int row = 0; row < puzzle.rows; row++) {
+
+                Shape shape = slicer.shape(col, row);
+
+                int tx = col * Puzzle.COL_SIZE + 20;
+                int ty = row * Puzzle.ROW_SIZE + 20;
+
+                AffineTransform tr = AffineTransform.getTranslateInstance(tx, ty);
+
+//                g2.setTransform(tr);
+
+            BufferedImage o =img.getSubimage(col*Puzzle.COL_SIZE, row*Puzzle.ROW_SIZE, Puzzle.COL_SIZE, Puzzle.ROW_SIZE);
+            g2.drawImage(o, null, 10, 10);
+
+                
+
             }
         }
+        g2.setTransform(old);
         
-//        int col = 1;
-//        int row = 1;
-//        Graphics2D g2 = (Graphics2D)g;
-//        g2.draw(slicing.shape(col, row));
-        
+
     }
 
 }
